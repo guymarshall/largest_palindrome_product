7391491aee10a22a59059f5f9bbb259ef19cd0fe
Guy Marshall <guymarshall9@gmail.com>
2022-05-07 09:03:08 +0100

    Now works - wrap number in str() instead of number[::-1]

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ def reverse_string(input):
 
 def is_palindrome(number):
     number_as_string = str(number)
-    return number_as_string == str(number[::-1])
+    return number_as_string == str(number)[::-1]
 
 def main():
     user_input = int(input("Enter a number of digits: "))
@@ -11,7 +11,7 @@ def main():
     largest_palindrome = 0
     product = 1
 
-    for i in range(max_number):
+    for i in range(max_number + 1):
         for j in range(max_number):
             product = i * j
             if is_palindrome(product) and product > largest_palindrome:
